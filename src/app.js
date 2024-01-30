2937e4478651ad1d4eb055e07d7082da55a40f96
Vargas-E <vargasivanezequiel@gmail.com>
2024-01-30 20:10:46 -0300

    Se agrego extended: true a app.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ const PORT = 8080;
 const productsRouter = require("./routes/products.router");
 const cartsRouter = require("./routes/carts.router");
 
-app.use(express.json())
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
